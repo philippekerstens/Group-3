a13f09bde1d856686398b59c9f960582d7cd2216
dylan-lomanto <86164867+dylan-lomanto@users.noreply.github.com>
2021-12-01 16:51:51 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_key='c33d1f14654c4d4a837e872efd48d432'
 
-connection_string = 'postgresql+psycopg2://postgres:YOURPASSWORDHERE@127.0.0.1:5432/COVID_Risk_Final_Project'
+connection_string = 'postgresql+psycopg2://postgres:YOURPASSWORDHERE@127.0.0.1:5432/COVID_Risk_Analysis'
 
